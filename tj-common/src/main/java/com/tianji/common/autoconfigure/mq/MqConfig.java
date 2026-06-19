bd28232cd9d1ce2efac1eb662c63141c4c367964
wuwenjincat <tomatocat1130@qq.com>
2026-06-20 01:19:53 +0800

    2026.6.20 提交修改

--- tj-common/src/main/java/com/tianji/common/autoconfigure/mq/MqConfig.java
+++ tj-common/src/main/java/com/tianji/common/autoconfigure/mq/MqConfig.java
@@ -6,12 +6,14 @@ import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ConditionalOnClass(value = {RocketMQTemplate.class})
+@EnableConfigurationProperties(RocketMQProperties.class)
 public class MqConfig {
 
     @Bean
